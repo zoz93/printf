9dba20b09ade594550940ebd715cd1c6139a2f20
zoz93 <abdelazizelhemaily@gmail.com>
2023-04-24 15:44:10 -0700

    abdelaziz 1.0

---
+++ AA_spec_printf.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * AA_spec_printf - function to choose the correct specifier format
+ * @format: format received from the printf func
+ * @args: list of arguments passed
+ * Return: 0 if success and -1 if '%' is the last char
+ */
+
+int AA_spec_printf(const char *format, va_list args)
+{
+	int p_state = 0, printed_len = 0;
+
+	while (format && *format)
+	{
+		if (p_state == 0)
+		{
+			if (*format == '%' && *(format + 1) == '\0')
+				return (-1);
+			else if (*format == '%' && *(format + 1) == '%')
+			{
+				_putchar('%');
+				printed_len++;
+				format++;
+			}
+			else if (*format == '%')
+				p_state = 1;
+			else
+			{
+				_putchar(*format);
+				printed_len++;
+			}
+		}
+		else if (p_state == 1)
+		{
+			AA_spec_mode(format, args, printed_len);
+				p_state = 0;
+		}
+		format++;
+	}
+	return (printed_len);
+}
